3e627318574660b3c779aca37189cbcd2ff432d4
ben198 <townsendben85@yahoo.co.uk>
2014-11-12 01:44:08 +0000

    progress made on coding keypad. work remains to be done.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,22 +3,24 @@
 	'use strict';
 
 	var button = document.getElementById('start'),
-		less = document.getElementById('btn-left'),
-		more = document.getElementById('btn-right'),
-	    field = document.getElementById('beats').value,
+		less   = document.getElementById('btn-left'),
+		more   = document.getElementById('btn-right'),
+		clear  = document.getElementById('clearBtn'),
+	    field  = document.getElementById('beats'),
 	    theInterval,
 	    beep = new Audio('sounds/beep.wav');
-	
-	button.onclick = function() {
-		if(this.innerText === 'Stop') {
-			this.innerText = 'Start';
+
+	var theSwitch = function theSwitch() {
+		if(button.innerText === 'Stop') {
+			button.innerText = 'Start';
 			clearInterval(theInterval);
 		} else {
-		// field = document.getElementById('beats').value;
-		// beats = field.value;
-		// field.value = '';
-		initialize(field);
+		initialize(field.value);
 		}
+	};
+	
+	button.onclick = function () {
+		return theSwitch();
 	};
 
 	var beeper = function beeper() {
@@ -34,19 +36,82 @@
 			beeper();
 			theInterval = setInterval(beeper, time);
 		}
-
 	};
 
 	less.onclick = function() {
-		var value = parseFloat(field) - 1;
-		field = value.toString();
-		document.getElementById('beats').value = field;
+		if (field.value === '') {
+			field.value = '31';
+		}
+		if (field.value > 30) {
+			var value = parseFloat(field.value) - 1;
+			field.value = value.toString();
+		}
 	};
 
 	more.onclick = function() {
-		var value = parseFloat(field) + 1;
-		field = value.toString();
-		document.getElementById('beats').value = field;
+		if (field.value === '') {
+			field.value = '29';
+		}
+		if (field.value < 230) {
+			var value = parseFloat(field.value) + 1;
+			field.value = value.toString();
+		}
+	};
+
+	clear.onclick = function() {
+		// field.value = '';
+		// theSwitch();
+		console.log(typeof(field.value));
+	};
+
+	function numberGenerator(number, storage) {
+		if (storage.length < 3) {
+			storage.push(number);
+		} else if (number > 0) {
+			storage.splice(0, 3, number);
+		}
+		return storage;
+	};
+
+	// function totalArray(array) {
+	// 	var i, total = 0;
+	// 	for (i = 0; i < array.length; i += 1) {
+	// 		total += array[i];
+	// 	}
+	// 	return total;
+	// };
+
+	var keypadHandler = function keypadHandler() {
+		var i,
+			box    = [],
+		    keypad = document.getElementsByClassName('keypadBtn');
+		for (i = 0; i < keypad.length; i += 1) {
+	    	keypad[i].onclick = function() {
+	    		var value  = parseFloat(field.value) + 1,
+	    		    num    = this.innerText,
+	    		    strike = numberGenerator(num, box),
+	    		    stroke = strike.map(Number),
+	    		    strack = stroke.join('');
+	    		    // tot    = totalArray(stroke);
+	    		if (strack < 231) {
+	    			smash(strike);
+	    		}
+	    		console.log(strack);
+	    		// box.push(this.innerText);
+	    		// smash(box);
+	    	}
+	    };
+	    // return function flipper() {
+	    // 	return box;
+	    // }
+	}();
+	
+
+	var smash = function smash(container) {
+		var f = container.join('');
+		// if (f < 231) {
+			field.value = f;
+		// }
 	};
 
 
